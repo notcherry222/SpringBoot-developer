6fb28f7595a74f744327d1ad7060a48b05763e5e
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-10-05 23:12:21 +0900

    추가 커밋

--- src/main/java/me/chaelim/springbootdeveloper/repository/ArticleRepository.java
+++ src/main/java/me/chaelim/springbootdeveloper/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package me.chaelim.springbootdeveloper.repository;
 
 import me.chaelim.springbootdeveloper.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }

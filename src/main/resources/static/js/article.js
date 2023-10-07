f6de25d4c885098716ce0647818030a67761c12b
notcherry222 <121853238+notcherry222@users.noreply.github.com>
2023-10-07 12:05:32 +0900

    게시글 수정 버튼 생성

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -1,5 +1,6 @@
 //삭제 기능
 const deleteButton = document.getElementById('delete-btn');
+const modifyButton = document.getElementById('modify-btn');
 
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
@@ -10,6 +11,31 @@ if (deleteButton) {
         .then(() => {
          alert('삭제가 완료되었습니다.');
          location.replace('/articles');
+        });
+    });
+}
+
+//수정 가능
+//id가 modify-btn인 엘리먼트 조회
+if(modifyButton){
+//클릭 이벤트가 감지되면 수정 API 요청
+modifyButton.addEventListener('click', event =>{
+    let params = new URLSearchParams(location,search);
+    let id = params.get('id');
+
+    fetch(`/api/articles/{id}`,{
+        method: 'PUT',
+        headers: {
+            "Content-type" : "application/json",
+        },
+        body: JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
+        })
+    })
+    .then(() => {
+        alert('수정이 완료되었습니다.');
+        location.replace('/articles/${id}');
         });
     });
 }
